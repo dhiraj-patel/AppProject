42750b12fa39f6d2564d4415e3c5982abade1d01
dhiraj-patel <dhirajsuperhero@gmail.com>
2015-08-26 00:02:55 -0400

    init

--- BlurrySideBar/ViewController.swift
+++ BlurrySideBar/ViewController.swift
@@ -1,25 +1,47 @@
 //
 //  ViewController.swift
-//  BlurrySideBar
+//  KaivalConnections
 //
-//  Created by Dhiraj Patel on 8/24/15.
+//  Created by Dhiraj Patel on 8/18/15.
 //  Copyright (c) 2015 Dhiraj Patel. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SideBarDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var sideBar:SideBar = SideBar()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sideBar = SideBar(sourceView: self.view, menuItems: ["Home","Upasanas","Bhajans","Stutis","Kaival Youth","About Us","Newsletter","Mandir News","Active Life"])
+        sideBar.delegate = self
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func sideBarDidSelectButtonAtIndex(index: Int) {
+        if index == 1 {
+            imageView.backgroundColor = UIColor.redColor()
+            imageView.image = nil
+        }
+        else if index == 2 {
+            imageView.backgroundColor = UIColor.clearColor()
+            imageView.image = UIImage(named: "background2")
+        }
+        else if index == 0 {
+            imageView.backgroundColor = UIColor.clearColor()
+            imageView.image = UIImage(named: "background2")
+        }
+        
+    }
+    
 }
-
